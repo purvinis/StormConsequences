141cdf15c4ca16b9f97007dc9496bbff151d7471
Purvinis <g.purvinis@gmail.com>
2020-09-23 18:09:16 -1000

    heat or tornado most fatal

--- StormDataAnalysis.R
+++ StormDataAnalysis.R
@@ -2,6 +2,8 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(R.utils)
+library(lubridate)
+library(lattice)
 
 print(Sys.info())
 print(Sys.info()['sysname'])
@@ -19,7 +21,7 @@ print(t2-t1)  #Time difference of 19.25788 secs
 t3 <- Sys.time()
 StormData <- read.csv("StormData.csv",sep = ",")
 t4 <- Sys.time()
-print(t4-t3)   #Time difference of 20.29467 secs
+print(t4-t3)   #Time difference of 20.29467 secs -24.37243 secs
 
 ## PROCESSING
 # info about the original variables can be found here: 
@@ -33,5 +35,55 @@ print(str(StormData, give.length = TRUE))
 # There are 985 unique events listed, but the documentation lists 48 Storm Events
 print(unique(StormData$EVTYPE))  # returns list of 985 events
 
-sd1 <-na_if(StormData,"")   #this is slow. Not sure I had to yet
-plot(StormData$EVTYPE,StormData$FATALITIES,)
+sd1 <-na_if(StormData[,],"")   #this is slow. Not sure I had to yet
+
+p1 <- plot.default(factor(sd1$EVTYPE),y = sd1$FATALITIES,ylim = c(0,200))
+png('plot1.png',width=480,height=480,units="px",bg = "transparent")
+print(p1) #make prettier, color the ranges
+dev.off()
+sd1$BGN_DATE <- date(sd1$BGN_DATE)
+# list the event types corresponding to fatalities between 15, 105
+Fatalpk1  <- filter(sd1,FATALITIES >15 & FATALITIES < 105)  # 87 observations
+Fatalpk2 <- filter(sd1,FATALITIES >105 & FATALITIES < 500)   # 3 tornado observations
+# Make sure all versions of Tornado are counted
+Fatalpk1$EVTYPE[agrep("TORNADO",Fatalpk1$EVTYPE,ignore.case = TRUE)] <- "Tornado"
+Fatalpk1Tornado <-Fatalpk1 %>%
+                       filter(EVTYPE == "Tornado") %>%
+                       select(EVTYPE) 
+print(summary(factor(Fatalpk1$EVTYPE)))
+# Data seems to indicate heat or tornado related events both cause fatalities. 
+# Narrow down these categories?
+
+# Want the event type to fit in one of the 48 official categories
+sd1$EVTYPE[agrep("TORNADO",sd1$EVTYPE,ignore.case = TRUE)] <- "Tornado"
+sd1$EVTYPE[agrep("HAIL",sd1$EVTYPE,ignore.case = TRUE)] <- "Hail"
+sd1$EVTYPE[agrep("SNOW | ICE ",sd1$EVTYPE,ignore.case = TRUE)] <- "Snow"
+sd1$EVTYPE[agrep("FUNNEL",sd1$EVTYPE,ignore.case = TRUE)] <- "Tornado"
+sd1$EVTYPE[agrep("HURRICANE",sd1$EVTYPE,ignore.case = TRUE)] <- "Hurricane"
+sd1$EVTYPE[agrep("FLOOD",sd1$EVTYPE,ignore.case = TRUE)] <- "Flood"
+sd1$EVTYPE[agrep("COLD | CHILL",sd1$EVTYPE,ignore.case = TRUE)] <- "Cold"
+sd1$EVTYPE[agrep("HEAT",sd1$EVTYPE,ignore.case = TRUE)] <- "Heat"
+sd1$EVTYPE[agrep("DUST",sd1$EVTYPE,ignore.case = TRUE)] <- "Dust"
+sd1$EVTYPE[agrep("RAIN | PRECIP | SUMMARY",sd1$EVTYPE,ignore.case = TRUE)] <- "Rain"
+sd1$EVTYPE[agrep("RAIN | PRECIP",sd1$EVTYPE,ignore.case = TRUE)] <- "Rain"
+sd1$EVTYPE[agrep("FIRE",sd1$EVTYPE,ignore.case = TRUE)] <- "Fire"
+sd1$EVTYPE[agrep("MUD",sd1$EVTYPE,ignore.case = TRUE)] <- "Mud"
+sd1$EVTYPE[agrep("VOLCANIC",sd1$EVTYPE,ignore.case = TRUE)] <- "Volcanic"
+
+print(length(unique(sd1$EVTYPE)))
+
+maxFatalIndex  <- which.max(sd1$FATALITIES)
+eventMaxFatal <- StormData$EVTYPE[maxFatalIndex]   # "heat" or "HEAT
+dateMaxFatal <- mdy_hms(StormData$BGN_DATE)[maxFatalIndex]  #1995-07-12
+remarksMaxFatal <- StormData$REMARKS[maxFatalIndex] "...july 12 - 16"
+#check events on these dates and same region?
+
+
+
+print(quantile(sd1$INJURIES))
+
+human <-as.data.frame(cbind(mdy_hms(sd1$BGN_DATE),sd1$EVTYPE,sd1$FATALITIES,sd1$INJURIES))
+length(unique(sd1$BGN_DATE))  #16225 unique days
+
+plot(human$V1,human$V3)
+
