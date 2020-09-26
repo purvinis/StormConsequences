93757a26c96f350bdd49322b93248dcaabb17386
Purvinis <g.purvinis@gmail.com>
2020-09-25 17:24:32 -1000

    good plots

--- StormDataAnalysisX.R
+++ StormDataAnalysisX.R
@@ -1,4 +1,6 @@
 install.packages("R.utils")
+install.packages ("cowplot")
+library(cowplot)
 
 library(dplyr)
 library(tidyr)
@@ -43,10 +45,72 @@ print(length(unique(sd1$EVTYPE)))
 print(str(sd1))
 
 # FATALITIES
-
-p1 <- plot.default(factor(sd1$EVTYPE),y = sd1$FATALITIES,ylim = c(0,200))
 png('plot1.png',width=480,height=480,units="px",bg = "transparent")
-print(p1) #make prettier, color the ranges
+p1 <- plot.default(factor(sd1$EVTYPE),y = sd1$FATALITIES,ylim = c(0,200))
+print(p1) #
+dev.off()
+
+# Repeat process for injuries
+png('plot2.png',width=480,height=480,units="px",bg = "transparent")
+p2 <- plot.default(factor(sd1$EVTYPE),y = sd1$INJURIES)
+print(p2) #make prettier, color the ranges
+dev.off()
+
+#FATALITIES AND INJURIES Together
+p3 <-ggplot(data =sd1,
+            aes(EVTYPE))+
+  ylab("Fatalities and Injuries")+
+  xlab("985 event types") +
+  geom_point(aes(y = INJURIES, colour = 'Injuries'))+
+  geom_point(aes(y = FATALITIES, colour = 'Fatalities'))+
+  theme(axis.text.x = element_blank())
+png('plot3.png',width=1080,height=360,units="px",bg = "transparent")
+print(p3) #make prettier, color the ranges
+dev.off()
+
+sd3 <- sd1
+#These matching criteria were iteratively determined
+sd3$EVTYPE[grep("TORN|SPOUT|WHIRL|GUSTNA|FUNNE|ROTAT",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Tornado"
+sd3$EVTYPE[grep("HAIL",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Hail"
+sd3$EVTYPE[grep("HEAT|HOT|WARM|HIGH TEMP",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Heat"
+sd3$EVTYPE[grep("WINT|BLIZZARD|SNOW|COLD|SLEET|FREEZ",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "snow"
+sd3$EVTYPE[grep("ICE|MIX|GLAZE|ICY",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Ice"
+sd3$EVTYPE[grep("THUNDER|TSTM|LIG|BURST|TURBU",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Tstorm"
+sd3$EVTYPE[grep("HURRIC|TROPICAL|TYPHO",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Hurricane "
+sd3$EVTYPE[grep("FLOOD|FLD|DAM BR",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Flood "
+sd3$EVTYPE[grep("SUMMARY|OTHER|[:?:]|NONE",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "tbd"
+sd3$EVTYPE[grep("FIRE|SMOKE",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Fire"
+sd3$EVTYPE[grep("MUD|LANDS|ROCK",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Mud"
+sd3$EVTYPE[grep("VOLCAN|VOG",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Volcanic"
+sd3$EVTYPE[grep("GLAZE|BLACK ICE",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Black_Ice"
+sd3$EVTYPE[grep("DROUGHT|DRY|DRIE",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Drought"
+sd3$EVTYPE[grep("WIND|WND",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Winds"  #includes some TSTM
+sd3$EVTYPE[grep("FROST|COOL",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Cool"  #includes frost
+sd3$EVTYPE[grep("SURF|SEA|TIDE|CURRENT|MARINE|TSUN|BEACH|WAVE|SWELL|SURGE|COASTAL",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Marine"
+sd3$EVTYPE[grep("RAIN|WET|PRECIP|SHOWER",sd3$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Rain"
+
+#add consolidated events to sd1 for nice facet plot
+sd4 <-data.frame(cbind(FATALITIES=sd1$FATALITIES,
+            INJURIES= sd1$INJURIES,
+            EVTYPE=sd1$EVTYPE,EVTYPEsub = sd3$EVTYPE))
+
+print(unique(sd4$EVTYPEsub))  #66 event categories
+
+#Try to get all the pre and post data together
+p5 <-ggplot(data =sd4,
+            aes(EVTYPEsub,INJURIES))+
+  ylab("Fatalities and Injuries")+
+  xlab("66 event catagories") +
+  geom_point(aes(y = as.numeric(INJURIES), colour = 'Injuries'))+
+  geom_point(aes(y = as.numeric(FATALITIES), colour = 'Fatalities'))+
+  theme(axis.text.x = element_text(angle = 90))
+png('plot5.png',width=1080,height=480,units="px",bg = "transparent")
+print(p5) #make prettier, color the ranges
+dev.off()
+
+p35 <-cowplot::plot_grid(p3,p5,nrow = 2, ncol = 1)
+png('plot35.png',width=1080,height=480,units="px",bg = "transparent")
+print(p35) #make prettier, color the ranges
 dev.off()
 
 # list the event types corresponding to fatalities between 15, 105
@@ -68,11 +132,7 @@ remarksMaxFatal <- StormData$REMARKS[maxFatalIndex] "...july 12 - 16"
 # REPORT ON THIS
 # --------------------------------------------------------------------------
 #--------------------------------------------------------------------------
-# Repeat process for injuries
-p2 <- plot.default(factor(sd1$EVTYPE),y = sd1$INJURIES)
-png('plot2.png',width=480,height=480,units="px",bg = "transparent")
-print(p2) #make prettier, color the ranges
-dev.off()
+
 
 sdinj <- sd1 %>% select(c(INJURIES, EVTYPE,REFNUM)) %>% filter(INJURIES > 50)
 print(summary (sdinj))   #421 observations
@@ -98,28 +158,49 @@ dateMaxInj <- mdy_hms(StormData$BGN_DATE)[maxInjIndex]  #1979-04-10
 # Clean up data set and combine similar event types
 
 sd2 <- sd1 %>% select(c(FATALITIES,INJURIES,EVTYPE,REFNUM,BGN_DATE))
+#Look at top 1000 injury count events
+injOrderInd <-order(sd2$INJURIES,decreasing = TRUE)  # gets index of max inj and sorts
+top1000Injdf <- sd2[injOrderInd[1:1000],]
+print(tail(cbind(top1000Injdf$INJURIES,top1000Injdf$EVTYPE),n  = 20))
+injCateg <- unique(top1000Injdf$EVTYPE) #45 evtypes returned
 
-# Want the event type to fit in one of the 48 official categories
-sd2$EVTYPE[agrep("torn | funnel",
-                 sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Tornado"
-sd2$EVTYPE[grep("HAIL",sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Hail"
+qplot(factor(EVTYPE),INJURIES,data =top1000Injdf)
+
+# Want the event types to be grouped 
+top1000Injdf$EVTYPE[grep("TORNA|SPOUT",top1000Injdf$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Tornado"
+top1000Injdf$EVTYPE[grep("HEAT",top1000Injdf$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Heat"
+top1000Injdf$EVTYPE[grep("WINTER|BLIZZARD|SNOW",top1000Injdf$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "snow"
+top1000Injdf$EVTYPE[grep("ICE|MIX|GLAZE",top1000Injdf$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Ice"
+top1000Injdf$EVTYPE[grep("THUNDER|TSTM|LIGHTN",top1000Injdf$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Tstorm"
+top1000Injdf$EVTYPE[grep("HURRIC|TROPICAL",top1000Injdf$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Hurricane "
+top1000Injdf$EVTYPE[grep("FLOOD",top1000Injdf$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Flood "
+#22 categories now
+p6 <-ggplot(data =top1000Injdf,
+            aes(EVTYPE,INJURIES))+
+            geom_point(aes(y = INJURIES), colour = 'red')+
+  geom_point(aes(y = FATALITIES), colour = 'blue')+
+  theme(axis.text.x = element_text(angle = 90))
+
+lessInjCats <-unique(top1000Injdf$EVTYPE)
+
+InjSums <-top1000Injdf %>% group_by(EVTYPE) %>%
+  summarise(totinj = sum(INJURIES)) %>% arrange(desc(totinj),by_group = TRUE)
+print(InjSums[1:10,])   #Tornado, Heat, Flood, snow...
+#Interesting that the single max death event did not appear in this list
+#Look again at total data with groupings. Call this dataset sd3
 
 
-sd2$EVTYPE[agrep("THUNDER | TSTM | TURBU | BURST ",
-                 sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Thunderstorm"
-sd2$EVTYPE[agrep("HURRICANE | TROPICAL",sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Hurricane"
-sd2$EVTYPE[grep("FLOOD | FLD",sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Flood"
-sd2$EVTYPE[agrep("DROUGHT | DRY",sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Drought"
-sd2$EVTYPE[grep("HEAT | HOT | WARM | HIGH TEMP",sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Heat"
-sd2$EVTYPE[grep("DUST",sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Dust"
-sd2$EVTYPE[grep("RAIN | PRECIP | SUMM | Summ",sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Rain"
-sd2$EVTYPE[grep("SUMMARY | OTHER | '?' ",sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "tbd"
-sd2$EVTYPE[grep("FIRE | SMOKE",sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Fire"
-sd2$EVTYPE[grep("MUD",sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Mud"
-sd2$EVTYPE[grep("VOLCAN | VOG ",sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Volcanic"
+#-----------------------------------------------------------------------------
+#Nothing good below (except link)
+#----------------------------------------------------
 
-sd2$EVTYPE[grep("SNOW | ICE | WINT | COLD | FROST |
-                 SLEET | FREEZ ",sd2$EVTYPE,ignore.case = TRUE,fixed = FALSE)] <- "Winter"
+
+injOrderInd <-order(sd2$INJURIES,decreasing = TRUE)  # gets index of max inj and sorts
+top10Injdf <- sd2[injOrderInd[1:10],]
+print(head(cbind(top10Injdf$INJURIES,top10Injdf$EVTYPE),n  = 20))
+
+
 
 # https://rstudio-pubs-static.s3.amazonaws.com/74603_76cd14d5983f47408fdf0b323550b846.html
 print(unique(sd2$EVTYPE))
+hist(as.factor(sd2$EVTYPE))
